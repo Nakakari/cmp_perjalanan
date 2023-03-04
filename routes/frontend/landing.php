560615f365cd2aabd9f1d0c1a6321a7467c788ab
Nakakari <sarinandhika@gmail.com>
2023-03-04 14:53:25 +0700

    [update]

--- routes/frontend/landing.php
+++ routes/frontend/landing.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ComproController;
 use App\Http\Controllers\trackingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [comproController::class, 'compro'])->name('compro');
-Route::get('tracking_resi', [comproController::class, 'comptrack']);
+// Route::get('/', [comproController::class, 'compro'])->name('compro');
+// Route::get('tracking_resi', [comproController::class, 'comptrack']);
 
 Route::get('tracking', [trackingController::class, 'index'])->name('get-tracking');
 Route::get('scantracking', [trackingController::class, 'scan'])->name('scan-tracking');
